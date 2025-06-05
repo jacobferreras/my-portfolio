fad429c1aa7f6b89f2c13d987e65b0c9f92085ab
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-05 20:13:42 +0800

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,109 @@
+"use client";
+import React from "react";
+import { motion } from "motion/react";
+import Particles from "@/components/ui/Particles/Particles";
+
+const page = () => {
+  return (
+    <>
+      <div className="bg-neutral-900 min-h-screen relative">
+        <div className="absolute inset-0 z-0 pointer-events-none">
+          <Particles
+            particleColors={["#ffffff", "#ffffff"]}
+            particleCount={200}
+            particleSpread={10}
+            speed={0.1}
+            particleBaseSize={100}
+            moveParticlesOnHover={true}
+            alphaParticles={false}
+            disableRotation={false}
+          />
+        </div>
+
+        <section
+          id="home"
+          className="flex justify-center items-center z-10 min-h-screen relative"
+        >
+          <div className="flex flex-col">
+            <div className="flex flex-row">
+              <h1 className="text-7xl text-white font-bold mr-4 mb-4">
+                Hi! I'm
+              </h1>
+              <h1 className="text-7xl text-blue-600 font-bold mb-4">
+                Rhobel Jacob Ferreras
+              </h1>
+            </div>
+            <h2 className="text-5xl text-white font-bold mb-4">
+              <span className="text-blue-600">👋</span>I am a Full Stack
+              Developer
+            </h2>
+            <div className="flex flex-row gap-x-4">
+              <motion.a
+                href="https://www.linkedin.com/in/jacob-ferreras-516a81273/"
+                target="_blank"
+                rel="noopener noreferrer"
+                whileHover={{ scale: 1.08 }}
+                whileTap={{ scale: 0.95 }}
+              >
+                <img
+                  src="/linkedin_icon.png"
+                  alt="LinkedIn"
+                  className="w-12 h-12"
+                />
+              </motion.a>
+              <motion.a
+                href="https://github.com/jacobferreras"
+                target="_blank"
+                rel="noopener noreferrer"
+                whileHover={{ scale: 1.08 }}
+                whileTap={{ scale: 0.95 }}
+              >
+                <img src="/github.png" alt="GItHub" className="w-12 h-12" />
+              </motion.a>
+            </div>
+          </div>
+
+          <div className="pl-4">
+            <motion.img
+              src="/my_picture.png"
+              alt="My Picture"
+              className="w-auto h-96"
+              whileHover={{ scale: 1.08 }}
+              whileTap={{ scale: 0.95 }}
+            />
+          </div>
+        </section>
+
+        <section
+          id="about"
+          className="flex justify-center items-center z-10 min-h-screen relative"
+        >
+          <h1 className="text-3xl text-white">About Page</h1>
+        </section>
+
+        <section
+          id="project"
+          className="flex justify-center items-center z-10 min-h-screen relative"
+        >
+          <h1 className="text-3xl text-white">About Page</h1>
+        </section>
+
+        <section
+          id="skills"
+          className="flex justify-center items-center z-10 min-h-screen relative"
+        >
+          <h1 className="text-3xl text-white">About Page</h1>
+        </section>
+
+        <section
+          id="contact"
+          className="flex justify-center items-center z-10 min-h-screen relative"
+        >
+          <h1 className="text-3xl text-white">About Page</h1>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default page;
---
+++ components/common/Navbar.tsx
@@ -0,0 +1,95 @@
+"use client";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="drawer font-body">
+      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col">
+        {/* Large Screen */}
+        <div className="navbar bg-transparent backdrop-blur-md fixed top-0 z-20 w-full lg:px-24 font-body pt-4">
+          <div className="flex-none lg:hidden ">
+            <label
+              htmlFor="my-drawer-3"
+              aria-label="open sidebar"
+              className="btn btn-square btn-ghost"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block h-6 w-6 stroke-current text-white"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
+          </div>
+          <div className="hidden lg:flex w-full justify-center">
+            <ul className="menu menu-horizontal flex justify-center font-semibold  font-body w-full text-white">
+              <li>
+                <Link href="#home">HOME</Link>
+              </li>
+              <li>
+                <Link href="#about">ABOUT</Link>
+              </li>
+              <li>
+                <Link href="#project">PROJECTS</Link>
+              </li>
+              <li>
+                <Link href="#skills">SKILLS</Link>
+              </li>
+              <li>
+                <Link href="#contact">CONTACT</Link>
+              </li>
+
+              <button className="btn btn-sm bg-blue-800 text-md">
+                {" "}
+                <img
+                  src="/download.png"
+                  alt="download Icon"
+                  className="h-4 w-4"
+                />
+                Resume
+              </button>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      {/* Small Screen */}
+      <div className="drawer-side z-[100] fixed top-0 left-0 w-full h-full font-body">
+        <label
+          htmlFor="my-drawer-3"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <div className="relative w-80 min-h-full h-full">
+          <ul className="menu bg-neutral-950 min-h-full w-80 p-4 pt-20 z-[101] relative shadow-2xl border-l border-neutral-800 font-body text-white">
+            <li>
+              <Link href="#home">HOME</Link>
+            </li>
+            <li>
+              <Link href="#about">ABOUT</Link>
+            </li>
+            <li>
+              <Link href="#projects">PROJECTS</Link>
+            </li>
+            <li>
+              <Link href="#skills">SKILLS</Link>
+            </li>
+            <li>
+              <Link href="#contact">CONTACT</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
