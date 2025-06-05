a62e75eb46d8f1b0794a81f07d27bc8943b9619e
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-06 01:42:32 +0800

    Refactor imports in ToogleTheme and useToggle components for clarity

--- components/ui/ToogleTheme.tsx
+++ components/ui/ToogleTheme.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React, { use } from "react";
-import { useState, useEffect } from "react";
+import React from "react";
 import useToggle from "@/hooks/useToggle";
 
 const ToogleTheme = () => {
--- hooks/useToggle.tsx
+++ hooks/useToggle.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+"use client";
 import { useState, useEffect } from "react";
 
 const useToggle = () => {
