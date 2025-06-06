3c971920cfacd4c846cb13fa17532d31304c659b
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-06 16:01:22 +0800

    Add Card component and enhance page layout with animations and new content

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { motion } from "motion/react";
 import Particles from "@/components/ui/Particles/Particles";
+import Card from "@/components/common/Card";
 
 const page = () => {
   return (
@@ -22,21 +23,54 @@ const page = () => {
 
         <section
           id="home"
-          className="flex justify-center items-center z-10 min-h-screen relative"
+          className="flex justify-center items-center z-10 min-h-screen relative px-12"
         >
           <div className="flex flex-col">
             <div className="flex flex-row">
-              <h1 className="text-7xl text-base-content font-bold mr-4 mb-4">
+              <motion.h1
+                className="text-7xl text-base-content font-bold mr-4 mb-4"
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1 }}
+              >
                 Hi! I'm
-              </h1>
-              <h1 className="text-7xl text-blue-600 font-bold mb-4">
+              </motion.h1>
+              <motion.h1
+                className="text-7xl text-blue-600 font-bold mb-4"
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1 }}
+              >
                 Rhobel Jacob Ferreras
-              </h1>
+              </motion.h1>
             </div>
-            <h2 className="text-5xl text-base-content font-bold mb-4">
-              <span className="text-blue-600">👋</span>I am a Full Stack
-              Developer
-            </h2>
+
+            <div className="flex flex-row gap-4">
+              <motion.h2
+                className="text-5xl text-base-content font-bold mb-4 "
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1 }}
+              >
+                <motion.span
+                  className="text-blue-600 "
+                  initial={{ opacity: 0, y: -50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1 }}
+                >
+                  👋
+                </motion.span>
+                I am a
+              </motion.h2>
+              <motion.h2
+                className="text-5xl text-base-content font-bold mb-4 underline decoration-blue-600 decoration-4"
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1 }}
+              >
+                Full Stack Developer
+              </motion.h2>
+            </div>
           </div>
 
           <div className="flex flex-col gap-y-4 ">
@@ -44,9 +78,12 @@ const page = () => {
               <motion.img
                 src="/my_picture.png"
                 alt="My Picture"
-                className="w-auto h-96"
+                className="w-auto h-86"
                 whileHover={{ scale: 1.08 }}
                 whileTap={{ scale: 0.95 }}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1.5 }}
               />
             </div>
             <div className="flex flex-row gap-x-4 justify-center">
@@ -56,6 +93,9 @@ const page = () => {
                 rel="noopener noreferrer"
                 whileHover={{ scale: 1.08 }}
                 whileTap={{ scale: 0.95 }}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1.5 }}
               >
                 <i className="bi bi-linkedin text-3xl"></i>
               </motion.a>
@@ -65,6 +105,9 @@ const page = () => {
                 rel="noopener noreferrer"
                 whileHover={{ scale: 1.08 }}
                 whileTap={{ scale: 0.95 }}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1.5 }}
               >
                 <i className="bi bi-github text-3xl"></i>
               </motion.a>
@@ -74,6 +117,9 @@ const page = () => {
                 rel="noopener noreferrer"
                 whileHover={{ scale: 1.08 }}
                 whileTap={{ scale: 0.95 }}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1.5 }}
               >
                 <i className="bi bi-instagram text-3xl"></i>
               </motion.a>
@@ -83,9 +129,73 @@ const page = () => {
 
         <section
           id="about"
-          className="flex justify-center items-center z-10 min-h-screen relative"
+          className="flex justify-center pt-32 z-10 relative gap-12"
         >
-          <h1 className="text-3xl text-base-content">About Me</h1>
+          <div className="flex flex-col items-start">
+            <motion.h1
+              className="text-5xl text-base-content font-bold underline decoration-blue-600 decoration-4 flex mb-4"
+              initial={{ opacity: 0, y: -50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
+            >
+              About Me
+            </motion.h1>
+
+            <motion.p
+              className="text-lg text-base-content mt-4 max-w-2xl text-justify font-semibold"
+              initial={{ opacity: 0, y: -50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
+            >
+              I am a Full Stack Developer with a passion for creating dynamic
+              and user-friendly web applications. My expertise spans both
+              front-end and back-end technologies, allowing me to build seamless
+              and efficient solutions from concept to deployment. I enjoy
+              learning new tools and frameworks, and I am always eager to take
+              on new challenges that help me grow as a developer.
+            </motion.p>
+            <div className="flex flex-row gap-4 mt-4">
+              <motion.div
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+              >
+                <Card
+                  icon="bi bi-mortarboard"
+                  title="Education"
+                  description="Bachelor of Science in Computer Science"
+                  firstInfo="STI San Jose Del Monte Bulacan"
+                  secondInfo="2021 - 2025"
+                />
+              </motion.div>
+              <motion.div
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+              >
+                <Card
+                  icon="bi bi-briefcase"
+                  title="Experience"
+                  description="Full Stack Developer Intern"
+                  firstInfo="Philippine Coast Guard WCEISC"
+                  secondInfo="March 2025 - May 2025"
+                />
+              </motion.div>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center  items-center pt-8">
+            <img
+              src="/Second_picture.png"
+              alt="my second picture"
+              className="w-full h-76 pt-8"
+            />
+            <div className="flex flex-row gap-4 my-4">
+              <div className="badge badge-outline">Developer</div>
+              <div className="badge badge-outline">Gamer</div>
+            </div>
+
+            <button className="btn bg-blue-600 text-base-content">
+              <i className="bi bi-box-arrow-down text-xl"></i>Download Resume
+            </button>
+          </div>
         </section>
 
         <section
---
+++ components/common/Card.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface CardProps {
+  icon?: string;
+  title?: string;
+  description?: string;
+  firstInfo?: string;
+  secondInfo?: string;
+}
+
+const Card = (props: CardProps) => {
+  return (
+    <div className="card w-96 bg-base-200 card-lg shadow-sm">
+      <div className="card-body">
+        <div className="flex flex-row gap-4">
+          {props.icon && <i className={`${props.icon} text-2xl`}></i>}
+          <h2 className="card-title font-bold text-xl">{props.title}</h2>
+        </div>
+        <p className="font-bold text-md">{props.description}</p>
+        <p>{props.firstInfo}</p>
+        <p>{props.secondInfo}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
