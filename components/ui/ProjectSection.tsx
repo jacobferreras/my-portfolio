29447d6d925f1ceb7a6239c30914ab8d4b27819b
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-07 16:54:45 +0800

    Add ProjectSection component with animated header and multiple ProjectCard instances

---
+++ components/ui/ProjectSection.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { motion } from "motion/react";
+import ProjectCard from "../common/ProjectCard";
+
+const ProjectSection = () => {
+  return (
+    <div>
+      {" "}
+      {/* Projects */}
+      <section
+        id="project"
+        className="flex justify-center items-center pt-32 z-10 relative gap-12"
+      >
+        <div className="flex flex-col items-start">
+          <motion.h1
+            className="text-5xl text-base-content font-bold underline decoration-blue-600 decoration-4 flex mb-8"
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+          >
+            Projects
+          </motion.h1>
+          <div className="flex flex-col lg:flex-row gap-4">
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default ProjectSection;
