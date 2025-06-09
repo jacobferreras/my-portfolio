4700b5a9782d364dd8fec8342ecc920c6b0c85f7
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-09 11:54:41 +0800

    Enhance text size responsiveness in ProjectSectionSmall component

--- components/ui/ProjectSectionSmall.tsx
+++ components/ui/ProjectSectionSmall.tsx
@@ -12,7 +12,7 @@ const ProjectSectionSmall = () => {
       >
         <div>
           <motion.h1
-            className="text-3xl text-base-content font-bold underline decoration-blue-600 decoration-4 mb-8"
+            className="text-3xl sm:text-4xl md:text-6xl text-base-content font-bold underline decoration-blue-600 decoration-4 mb-8"
             initial={{ opacity: 0, y: -50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1 }}
