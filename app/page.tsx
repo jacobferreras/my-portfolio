87fb1e9b09e61026a1f65f0e3477680204819a70
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-10 00:00:43 +0800

    Refactor page layout to include Skills and Contact sections for improved content organization

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,12 @@ import Particles from "@/components/ui/Particles/Particles";
 import HomeSectionLarge from "@/components/ui/HomeSectionLarge";
 import AboutSectionLarge from "@/components/ui/AboutSectionLarge";
 import ProjectSectionLarge from "@/components/ui/ProjectSectionLarge";
-import ProjectSectionSmall from "@/components/ui/ProjectSectionSmall";
+import SkillsSectionLarge from "@/components/ui/SkillsSectionLarge";
+import ContactSectionLarge from "@/components/ui/ContactSectionLarge";
 
 const page = () => {
   return (
-    <div className="bg-base-100 justify-center items-center min-h-screen relative">
+    <div className="bg-base-100 justify-center items-center min-h-screen relative gap-y-12">
       <div className="absolute inset-0 z-0 pointer-events-none">
         <Particles
           particleColors={["#ffffff", "#ffffff"]}
@@ -27,13 +28,23 @@ const page = () => {
       </div>
 
       {/* About */}
-      <div className="justify-center items-center">
+      <div className="justify-center items-center max-w-7xl mx-auto">
         <AboutSectionLarge />
       </div>
 
       {/* Projects */}
-      <div className="justify-center items-center">
+      <div className="justify-center items-center max-w-7xl mx-auto">
         <ProjectSectionLarge />
+      </div>
+
+      {/* Skills */}
+      <div className="justify-center items-center max-w-7xl mx-auto">
+        <SkillsSectionLarge />
+      </div>
+
+      {/* Contact */}
+      <div className="justify-center items-center max-w-7xl mx-auto">
+        <ContactSectionLarge />
       </div>
     </div>
   );
