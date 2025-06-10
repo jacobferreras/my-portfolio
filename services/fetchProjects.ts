18d400c2343017b88bf8ac2767a6fedaa3165301
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-10 22:09:43 +0800

    Update API endpoint in fetchProjects function to use production URL

--- services/fetchProjects.ts
+++ services/fetchProjects.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export const fetchProjects = async () => {
   try {
-    const response = await axios.get("http://localhost:3000/api/projects");
+    const response = await axios.get(
+      "https://my-portfolio-five-roan-50.vercel.app/api/projects"
+    );
     return response.data;
   } catch (error) {
     console.error("Error fetching projects:", error);
