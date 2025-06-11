a7cf7c17b3b8ae5a56ce89eb2fcb1b0480e2618e
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2025-06-11 12:38:01 +0800

    Add dotenv package and update fetchProjects to use NEXT_PUBLIC_API_KEY

--- services/fetchProjects.ts
+++ services/fetchProjects.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const fetchProjects = async () => {
   try {
-    const response = await axios.get(`${process.env.API_KEY}/api/projects`);
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_KEY}/api/projects`
+    );
     return response.data;
   } catch (error) {
     console.error("Error fetching projects:", error);
