a1ca14e58a842cc05e8f9a7c3f9c729c2f52449a
Rhobel Jacob S. Ferreras <152048667+jacobferreras@users.noreply.github.com>
2026-03-11 21:50:39 +0800

    Refactor HomeSectionLarge component for improved layout and styling; update profile image and add new image

--- components/ui/HomeSectionLarge.tsx
+++ components/ui/HomeSectionLarge.tsx
@@ -12,60 +12,52 @@ const HomeSectionLarge = () => {
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
       >
-        <div className="flex flex-col xl:mr-4">
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:mb-4">
-            <motion.h1
-              className="text-3xl sm:text-4xl lg:text-5xl xl:text-6xl text-base-content font-bold mr-2"
-              initial={{ opacity: 0, y: -50 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1 }}
-            >
-              Hi! I&apos;m
-            </motion.h1>
-            <motion.h1
-              className="text-3xl sm:text-5xl lg:text-5xl xl:text-6xl text-blue-600 font-bold"
-              initial={{ opacity: 0, y: -50 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1 }}
-            >
-              Rhobel Jacob Ferreras
-            </motion.h1>
-          </div>
+        <div className="flex flex-col px-4 xl:mr-4">
+          <motion.h2
+            className="text-[#a3a3a3] text-xl sm:text-4xl xl:text-5xl font-serif font-bold text-center mb-4"
+            initial={{ opacity: 0, y: -50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+          >
+            Jacob Ferreras
+          </motion.h2>
 
-          <div className="flex flex-row gap-2 md:gap-4">
-            <motion.h2
-              className="text-xl sm:text-4xl xl:text-5xl text-base-content font-bold lg:mb-4 "
-              initial={{ opacity: 0, y: -50 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1 }}
-            >
-              <motion.span
-                className="text-blue-600 "
-                initial={{ opacity: 0, y: -50 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1 }}
-              >
-                👋
-              </motion.span>
-              I am a
-            </motion.h2>
-            <motion.h2
-              className="text-xl sm:text-4xl xl:text-5xl text-base-content font-bold mb-4 underline decoration-blue-600 decoration-4"
-              initial={{ opacity: 0, y: -50 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1 }}
-            >
-              Full Stack Developer
-            </motion.h2>
+          <motion.h1
+            className="text-3xl sm:text-5xl lg:text-5xl xl:text-6xl text-[#fafafa] font-bold text-center mb-4"
+            initial={{ opacity: 0, y: -50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+          >
+            Full Stack Developer
+          </motion.h1>
+
+          <motion.h2
+            className="text-lg sm:text-xl  font-inter font-extralight text-center mb-4"
+            initial={{ opacity: 0, y: -50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+          >
+            Transforming ideas into scalable web applications with clean and
+            efficient code.
+          </motion.h2>
+
+          <div className="flex flex-col md:flex-row gap-4 justify-center items-center mb-4">
+            <motion.button className="btn rounded-lg bg-[#454545] w-full md:w-auto">
+              Contact Me
+            </motion.button>
+
+            <motion.button className="btn-ghost rounded-lg  w-full md:w-auto">
+              View Projects
+            </motion.button>
           </div>
         </div>
 
         <div className="flex flex-col gap-y-4 ">
           <div className="">
             <motion.img
-              src="/my_picture.png"
+              src="/Jacob.png"
               alt="My Picture"
-              className="max-w-72 h-72 md:h-auto md:max-w-86 rounded-full"
+              className="max-w-auto h-98 md:h-auto md:max-w-86 rounded-lg border-accent-content mb-4"
               whileHover={{ scale: 1.08 }}
               whileTap={{ scale: 0.95 }}
               initial={{ opacity: 0, y: 20 }}
